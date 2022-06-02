13c2b0a98c239bf56c5a404af4327832fe3815de
matheus costa <matheuscosta.tech@gmail.com>
2022-06-01 22:08:52 -0300

    Parsing json data

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const app = express();
 
 app.get('/', (req, res) => {
 	const apiKey = 'b8918abe720101b4392cb8b50fc524de';
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`;
 
 	https.get(url, (response) => {
-		console.log(response.statusCode);
+        response.on('data', (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+
+            console.log(weatherData);
+            console.log(temp);
+            console.log(weatherDescription);
+        });
 	});
 });
 
