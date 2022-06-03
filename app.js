87a3fa59f9b264bd31c40e7f817ac00ae7bf7577
Matheus-IT <matheuscosta.tech@gmail.com>
2022-06-02 21:47:10 -0300

    Allow to enter a city name

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+app.post('/', (req, res) => {
+	function capitalize(string) {
+		const firstLetter = string[0].toUpperCase();
+		return firstLetter + string.slice(1);
+	}
+
+	const { cityName } = req.body;
 	const apiKey = 'b8918abe720101b4392cb8b50fc524de';
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=${apiKey}`;
-
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+	
 	https.get(url, (response) => {
-        response.on('data', (data) => {
-            const weatherData = JSON.parse(data);
-            const temperature = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            const iconName = weatherData.weather[0].icon;
-            const iconURL = `http://openweathermap.org/img/wn/${iconName}@2x.png`;
-
-            res.write(`<p>The weather is currently${weatherDescription}</p>`);
-            res.write(`<h1>The temperature in London is ${temperature}</h1>`);
-            res.write(`<img src="${iconURL}" alt="">`);
-            res.send();
-        });
+	    response.on('data', (data) => {
+	        const weatherData = JSON.parse(data);
+	        const temperature = weatherData.main.temp;
+	        const weatherDescription = weatherData.weather[0].description;
+	        const iconName = weatherData.weather[0].icon;
+	        const iconURL = `http://openweathermap.org/img/wn/${iconName}@2x.png`;
+	
+	        res.write(`<p>The weather is currently${weatherDescription}</p>`);
+	        res.write(`<h1>The temperature in ${capitalize(cityName)} is ${temperature}</h1>`);
+	        res.write(`<img src="${iconURL}" alt="">`);
+	        res.send();
+	    });
 	});
 });
+
 
 const port = 3000;
 app.listen(port, () => {
